c29e56289779828464b1418d86eee31110768132
Keksgauner <keksgauner@gmail.com>
2022-09-16 00:31:03 +0200

    Fix in LobbySwitcher if a service starting

--- src/main/java/de/mxscha/endernationlobby/listener/items/LobbySwitcher.java
+++ src/main/java/de/mxscha/endernationlobby/listener/items/LobbySwitcher.java
@@ -15,6 +15,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 
@@ -36,22 +37,29 @@ public class LobbySwitcher implements Listener {
                     int position = 0;
                     WrapperGeneralCloudServiceProvider wrapperNodeInfoProvider = new WrapperGeneralCloudServiceProvider(Wrapper.getInstance());
                     for(ServiceInfoSnapshot service : wrapperNodeInfoProvider.getCloudServices("Lobby")) {
+                        // Fix of java.util.NoSuchElementException: No value present
+                        // We cannot get infos of the server is the server not connected
+                        if(!service.isConnected()) {
+                            continue; // Server is offline
+                        }
                         // get all infos
                         String name = service.getName().replaceAll("-", " ");
                         int currentPlayers = service.getProperty(BridgeServiceProperty.ONLINE_COUNT).get();
                         int maxPlayers = service.getProperty(BridgeServiceProperty.MAX_PLAYERS).get();
-                        boolean isOnline = service.getProperty(BridgeServiceProperty.IS_ONLINE).get();
                         boolean isFull = service.getProperty(BridgeServiceProperty.IS_FULL).get();
                         boolean isStarting = service.getProperty(BridgeServiceProperty.IS_STARTING).get();
                         boolean isEmpty = service.getProperty(BridgeServiceProperty.IS_EMPTY).get();
 
                         // Check if player on this server
                         boolean isPlayerOnline = false;
-                        Collection<ServicePlayer> players = service.getProperty(BridgeServiceProperty.PLAYERS).get();
-                        for (ServicePlayer servicePlayer : players) // loop through all players
-                            if(servicePlayer.getUniqueId().equals(player.getUniqueId())) // check if they're the uuid of the player
-                                isPlayerOnline = true; // yes there is the player
 
+                        // Only if the server not empty and not starting
+                        if(!isEmpty && !isStarting) {
+                            Collection<ServicePlayer> players = service.getProperty(BridgeServiceProperty.PLAYERS).get();
+                            for (ServicePlayer servicePlayer : players) // loop through all players
+                                if (servicePlayer.getUniqueId().equals(player.getUniqueId())) // check if they're the uuid of the player
+                                    isPlayerOnline = true; // yes there is the player
+                        }
 
                         /* What to do:
                          * Player Joined Glowestone
@@ -69,19 +77,17 @@ public class LobbySwitcher implements Listener {
                         }
 
                         // set to invenory
-                        // 1. is the service online?
-                        if(isOnline) {
-                            // 2. is the server starting?
-                            if(isStarting) {
-                                // Starting
-                                inventory.setItem(position,
-                                        new ItemCreator(Material.LIGHT_GRAY_CONCRETE)
-                                                .setName(name)
-                                                .setLore("§aServer Starting")
-                                                .toItemStack());
+                        // 1. is the server starting?
+                        if(isStarting) {
+                            // Starting
+                            inventory.setItem(position,
+                                    new ItemCreator(Material.LIGHT_GRAY_CONCRETE)
+                                            .setName(name)
+                                            .setLore("7Players §aStartet")
+                                            .toItemStack());
 
-                            } else
-                            // 3. is the player on this server?
+                        } else
+                            // 2. is the player on this server?
                             if(isPlayerOnline) {
                                 // The Player is on this server && send normal Info
                                 inventory.setItem(position,
@@ -90,43 +96,35 @@ public class LobbySwitcher implements Listener {
                                                 .setLore("§cPlayers " + currentPlayers + "/" + maxPlayers)
                                                 .toItemStack());
                             } else
-                            // 4. is the service full?
-                            if(isFull) {
-                                // Full
-                                inventory.setItem(position,
-                                        new ItemCreator(Material.REDSTONE)
-                                                .setName(name)
-                                                .setLore("§cServer Full")
-                                                .toItemStack());
+                                // 3. is the service full?
+                                if(isFull) {
+                                    // Full
+                                    inventory.setItem(position,
+                                            new ItemCreator(Material.REDSTONE)
+                                                    .setName("§c" + name)
+                                                    .setLore("§7Players §cServer Full")
+                                                    .toItemStack());
 
-                            } else
-                            // 5. is the service empty?
-                            if(isEmpty) {
-                                // Empty
-                                inventory.setItem(position,
-                                        new ItemCreator(Material.SUGAR)
-                                                .setName(name)
-                                                .setLore("§cOnline Empty")
-                                                .toItemStack());
+                                } else
+                                    // 4. is the service empty?
+                                    if(isEmpty) {
+                                        // Empty
+                                        inventory.setItem(position,
+                                                new ItemCreator(Material.SUGAR)
+                                                        .setName("§e" +name)
+                                                        .setLore("§7Players §7Leer")
+                                                        .toItemStack());
 
-                            } else
-                            // 6. Send normal server info
-                            {
-                                // Normal server info
-                                inventory.setItem(position,
-                                        new ItemCreator(Material.SUGAR)
-                                                .setName(name)
-                                                .setLore("§cPlayers " + currentPlayers + "/" + maxPlayers)
-                                                .toItemStack());
-                            }
-                        } else {
-                            // Server Offline
-                            inventory.setItem(position,
-                                    new ItemCreator(Material.RED_CONCRETE)
-                                            .setName(name)
-                                            .setLore("§cOffline")
-                                            .toItemStack());
-                        }
+                                    } else
+                                        // 5. Send normal server info
+                                        {
+                                            // Normal server info
+                                            inventory.setItem(position,
+                                                    new ItemCreator(Material.SUGAR)
+                                                            .setName("§e" + name)
+                                                            .setLore("§7Players §a" + currentPlayers + "§7/§c" + maxPlayers)
+                                                            .toItemStack());
+                                        }
                     }
                     // Keksgauner END
 
@@ -134,6 +132,7 @@ public class LobbySwitcher implements Listener {
                 }
             }
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
