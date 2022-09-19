80290542118e9995c5a0c9c76651b2c97590d5e0
Keksgauner <keksgauner@gmail.com>
2022-09-19 04:25:37 +0200

    updated: set ServerWechslerListener to a old version

--- src/main/java/de/mxscha/endernationlobby/listener/ServerWechslerListener.java
+++ src/main/java/de/mxscha/endernationlobby/listener/ServerWechslerListener.java
@@ -25,7 +25,6 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class ServerWechslerListener implements Listener {
@@ -41,14 +40,130 @@ public class ServerWechslerListener implements Listener {
                 Inventory inventory = Bukkit.createInventory(null, 9 * 6, "§8» §6§lServer Wechsler");
                 fill(inventory);
 
+                // Server info
+                int positionX = 1; // Starting at 1
+                int positionY = 0;
+                int position = 0;
+                WrapperGeneralCloudServiceProvider wrapperNodeInfoProvider = new WrapperGeneralCloudServiceProvider(Wrapper.getInstance());
+                for(ServiceInfoSnapshot service : wrapperNodeInfoProvider.getStartedCloudServices()) {
+                    // get all infos
+                    String nameRaw;
+                    String name;
+                    int currentPlayers;
+                    int maxPlayers;
+                    boolean isFull;
+                    boolean isStarting;
+                    boolean isEmpty;
 
+                    // No error if the server starting
+                    try {
+                        // We cannot get infos of the server is the server not connected
+                        nameRaw = service.getName();
+                        name = service.getName().replaceAll("-", " ");
+                        currentPlayers = service.getProperty(BridgeServiceProperty.ONLINE_COUNT).get();
+                        maxPlayers = service.getProperty(BridgeServiceProperty.MAX_PLAYERS).get();
+                        isFull = service.getProperty(BridgeServiceProperty.IS_FULL).get();
+                        isStarting = service.getProperty(BridgeServiceProperty.IS_STARTING).get();
+                        isEmpty = service.getProperty(BridgeServiceProperty.IS_EMPTY).get();
+                    }catch (NoSuchElementException ex) {
+                        // NoSuchElementException – if no value is present
+                        continue; // skip item
+                    }
+
+
+                    // Check if player on this server
+                    boolean isPlayerOnline = false;
+
+                    // Only if the server not empty and not starting
+                    if(!isEmpty && !isStarting) {
+                        Collection<ServicePlayer> players = service.getProperty(BridgeServiceProperty.PLAYERS).get();
+                        for (ServicePlayer servicePlayer : players) // loop through all players
+                            if (servicePlayer.getUniqueId().equals(player.getUniqueId())) // check if they're the uuid of the player
+                                isPlayerOnline = true; // yes there is the player
+                    }
+
+                    /* What to do:
+                     * Player Joined Glowestone
+                     * If service online Sugar
+                     * If service full Redstone
+                     */
+
+                    // Count up because it is a new service
+                    if(positionX > 9) { // count y up with at 8
+                        positionY++;
+                        positionX = 1;
+                    } else
+                        positionX++;
+
+                    // Math xd. y * 9 and x the location
+                    position = (positionY * 9) + positionX;
+
+                    // Only 42 Servers allowed
+                    if(position >= 42) {
+                        player.sendMessage("§Error! §7Only 42 Servers are allowed. Report it to an Administrator!");
+                        continue; // Skip this service
+                    }
+
+                    ItemStack itemStack = null;
+
+                    // set to invenory
+                    // 1. is the server starting?
+                    if(isStarting) {
+                        // Starting
+                        itemStack = new ItemCreator(Material.LIGHT_GRAY_CONCRETE)
+                                .setName("§e" + name)
+                                .setLore("7Players §aStartet")
+                                .toItemStack();
+
+                    } else
+                        // 2. is the player on this server?
+                        if(isPlayerOnline) {
+                            // The Player is on this server && send normal Info
+                            itemStack = new ItemCreator(Material.GLOWSTONE_DUST)
+                                    .setName("§e" + name)
+                                    .setLore("§7Players §a" + currentPlayers + "§7/§c" + maxPlayers)
+                                    .toItemStack();
+                        } else
+                            // 3. is the service full?
+                            if(isFull) {
+                                // Full
+                                itemStack = new ItemCreator(Material.REDSTONE)
+                                        .setName("§c" + name)
+                                        .setLore("§7Players §cServer Full")
+                                        .toItemStack();
+
+                            } else
+                                // 4. is the service empty?
+                                if(isEmpty) {
+                                    // Empty
+                                    itemStack = new ItemCreator(Material.SUGAR)
+                                            .setName("§e" + name)
+                                            .setLore("§7Players §7Leer")
+                                            .toItemStack();
+
+                                } else
+                                // 5. Send normal server info
+                                {
+                                    // Normal server info
+                                    itemStack = new ItemCreator(Material.SUGAR)
+                                            .setName("§e" + name)
+                                            .setLore("§7Players §a" + currentPlayers + "§7/§c" + maxPlayers)
+                                            .toItemStack();
+                                }
+
+                    // get item meta
+                    ItemMeta itemMeta = itemStack.getItemMeta();
+                    // Add to item server info (name)
+                    itemMeta.getPersistentDataContainer().set(NamespacedKey.fromString("server"), PersistentDataType.STRING, nameRaw);
+                    // set the server info into the item
+                    itemStack.setItemMeta(itemMeta);
 
                     // set the item to the inventory
-                    //inventory.setItem(position, itemStack);
+                    inventory.setItem(position, itemStack);
                 }
 
-                //player.openInventory(inventory);
-
+                player.openInventory(inventory);
+            }
 
             if (event.getView().getTitle().equals("§8» §6§lServer Wechsler")) {
                 // get item infos
@@ -79,129 +194,5 @@ public class ServerWechslerListener implements Listener {
         for (int i = 0; i < inventory.getSize(); i++) {
             inventory.setItem(i, new ItemCreator(Material.GRAY_STAINED_GLASS_PANE).setName("").toItemStack());
         }
-    }
-
-    // key servers second key position and the item
-    private HashMap<String, HashMap<Integer, ItemStack>> getGroups(Player player) {
-        HashMap<String, HashMap<Integer, ItemStack>> groups = new HashMap<>();
-        // Server info
-        int position = 0;
-        WrapperGeneralCloudServiceProvider wrapperNodeInfoProvider = new WrapperGeneralCloudServiceProvider(Wrapper.getInstance());
-        for(ServiceInfoSnapshot service : wrapperNodeInfoProvider.getStartedCloudServices()) {
-            // get all infos
-            String group;
-            String nameRaw;
-            String name;
-            int currentPlayers;
-            int maxPlayers;
-            boolean isFull;
-            boolean isStarting;
-            boolean isEmpty;
-
-            // No error if the server starting
-            try {
-                // We cannot get infos of the server is the server not connected
-                group = service.getName().split("-")[0];
-                nameRaw = service.getName();
-                name = service.getName().replaceAll("-", " ");
-                currentPlayers = service.getProperty(BridgeServiceProperty.ONLINE_COUNT).get();
-                maxPlayers = service.getProperty(BridgeServiceProperty.MAX_PLAYERS).get();
-                isFull = service.getProperty(BridgeServiceProperty.IS_FULL).get();
-                isStarting = service.getProperty(BridgeServiceProperty.IS_STARTING).get();
-                isEmpty = service.getProperty(BridgeServiceProperty.IS_EMPTY).get();
-            } catch (NoSuchElementException ex) {
-                // NoSuchElementException – if no value is present
-                continue; // skip item
-            }
-
-
-            // Check if player on this server
-            boolean isPlayerOnline = false;
-
-            // Only if the server not empty and not starting
-            if (!isEmpty && !isStarting) {
-                Collection<ServicePlayer> players = service.getProperty(BridgeServiceProperty.PLAYERS).get();
-                for (ServicePlayer servicePlayer : players) // loop through all players
-                    if (servicePlayer.getUniqueId().equals(player.getUniqueId())) // check if they're the uuid of the player
-                        isPlayerOnline = true; // yes there is the player
-            }
-
-            /* What to do:
-             * Player Joined Glowestone
-             * If service online Sugar
-             * If service full Redstone
-             */
-
-            // Count up because it is a new service
-            /*if (positionX > 9) { // count y up with at 8
-                positionY++;
-                positionX = 1;
-            } else
-                positionX++;
-
-            // Math xd. y * 9 and x the location
-            position = (positionY * 9) + positionX;
-
-            // Only 42 Servers allowed
-            if (position >= 42) {
-                player.sendMessage("§Error! §7Only 42 Servers are allowed. Report it to an Administrator!");
-                continue; // Skip this service
-            }*/
-
-            ItemStack itemStack = null;
-
-            // set to invenory
-            // 1. is the server starting?
-            if (isStarting) {
-                // Starting
-                itemStack = new ItemCreator(Material.LIGHT_GRAY_CONCRETE)
-                        .setName("§e" + name)
-                        .setLore("7Players §aStartet")
-                        .toItemStack();
-
-            } else
-                // 2. is the player on this server?
-                if (isPlayerOnline) {
-                    // The Player is on this server && send normal Info
-                    itemStack = new ItemCreator(Material.GLOWSTONE_DUST)
-                            .setName("§e" + name)
-                            .setLore("§7Players §a" + currentPlayers + "§7/§c" + maxPlayers)
-                            .toItemStack();
-                } else
-                    // 3. is the service full?
-                    if (isFull) {
-                        // Full
-                        itemStack = new ItemCreator(Material.REDSTONE)
-                                .setName("§c" + name)
-                                .setLore("§7Players §cServer Full")
-                                .toItemStack();
-
-                    } else
-                        // 4. is the service empty?
-                        if (isEmpty) {
-                            // Empty
-                            itemStack = new ItemCreator(Material.SUGAR)
-                                    .setName("§e" + name)
-                                    .setLore("§7Players §7Leer")
-                                    .toItemStack();
-
-                        } else
-                        // 5. Send normal server info
-                        {
-                            // Normal server info
-                            itemStack = new ItemCreator(Material.SUGAR)
-                                    .setName("§e" + name)
-                                    .setLore("§7Players §a" + currentPlayers + "§7/§c" + maxPlayers)
-                                    .toItemStack();
-                        }
-
-            // get item meta
-            ItemMeta itemMeta = itemStack.getItemMeta();
-            // Add to item server info (name)
-            itemMeta.getPersistentDataContainer().set(NamespacedKey.fromString("server"), PersistentDataType.STRING, nameRaw);
-            // set the server info into the item
-            itemStack.setItemMeta(itemMeta);
-        }
-        return groups;
     }
 }
