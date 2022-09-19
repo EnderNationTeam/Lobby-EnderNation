32f3c564577b111371257aafb8386686475e0446
marce <marcelmaj2@gmail.com>
2022-09-19 02:39:00 +0200

    Update Commpas Games

--- src/main/java/de/mxscha/endernationlobby/listener/items/Compass.java
+++ src/main/java/de/mxscha/endernationlobby/listener/items/Compass.java
@@ -11,6 +11,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -36,27 +37,26 @@ public class Compass implements Listener {
                     out.writeUTF("EndOfLife-1");
                     player.sendPluginMessage(LobbyCore.getInstance(), "BungeeCord", out.toByteArray());
                     int eofPlayerCount = PluginMessagesListener.getPlayerCountEndOfLife();
-                    inventory.setItem(22, new ItemCreator(Material.SUNFLOWER)
+                    inventory.setItem(22, new ItemCreator(Material.MAGMA_CREAM)
+                            .addEnchantment(Enchantment.KNOCKBACK,1,true)
                             .setName("§8» §6§lSpawn")
                             .setLore("§8● §7Klicke um dich zum §6§lSpawn §7zu teleportieren!")
                             .toItemStack());
-                    inventory.setItem(3, new ItemCreator(Material.LIGHT_BLUE_BED)
-                            .setName("§8» §b§lBedWars")
-                            .setLore("§8● §7Klicke um dich zu §b§lBedWars §7zu teleportieren!")
-                            .toItemStack());
-                    inventory.setItem(5, new ItemCreator(Material.LEATHER_CHESTPLATE)
-                            .setColor(Color.RED)
-                            .addItemFlag(ItemFlag.HIDE_DYE)
-                            .addItemFlag(ItemFlag.HIDE_ATTRIBUTES)
-                            .setName("§8» §c§lTTT")
-                            .setLore("§8● §7Klicke um dich zu §c§lTTT §7zu teleportieren!")
-                            .toItemStack());
-                    inventory.setItem(10, new ItemCreator(Material.WITHER_ROSE)
+                    inventory.setItem(3, new ItemCreator(Material.WITHER_ROSE)
                             .setName("§8» §9§lEnd§f§lOf§a§lLife")
                             .setLore("§8● §7Spieler Online§8: §b" + eofPlayerCount, "","§8● §7Klicke um dich zu §9§lEnd§f§lOf§a§lLife §7zu teleportieren!")
                             .toItemStack());
-                    inventory.setItem(16, new ItemCreator(Material.RED_STAINED_GLASS_PANE)
-                            .setName("§c§lIn Arbeit!")
+                    inventory.setItem(5, new ItemCreator(Material.IRON_SWORD)
+                            .setName("§8» §c§lMurder Mystery")
+                            .setLore("§8● §7Klicke um dich zu §c§lMurder Mystery §7zu teleportieren!")
+                            .toItemStack());
+                    inventory.setItem(10, new ItemCreator(Material.GRASS_BLOCK)
+                            .setName("§8» §a§lBuild §c§lBattle")
+                            .setLore("§8● §7Klicke um dich zu §a§lBuild §c§lBattle §7zu teleportieren!")
+                            .toItemStack());
+                    inventory.setItem(16, new ItemCreator(Material.NETHERITE_PICKAXE)
+                            .setName("§8» §b§lSmash")
+                            .setLore("§8● §7Klicke um dich zu §b§lSmash §7zu teleportieren!")
                             .toItemStack());
                     inventory.setItem(28, new ItemCreator(Material.RED_STAINED_GLASS_PANE)
                             .setName("§c§lIn Arbeit!")
@@ -86,20 +86,25 @@ public class Compass implements Listener {
                         player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 0.5f, 1);
                         player.sendMessage(MessageManager.Prefix + "§7Du bist nun beim §6§lSpawn§7!");
                         break;
-                    case "§8» §b§lBedWars":
-                        player.teleport(new ConfigLocationUtil("BedWars").loadLocation());
+                    case "§8» §b§lSmash":
+                        player.teleport(new ConfigLocationUtil("Smash").loadLocation());
                         player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 0.5f, 1);
-                        player.sendMessage(MessageManager.Prefix + "§7Du bist nun bei §b§lBedWars§7!");
+                        player.sendMessage(MessageManager.Prefix + "§7Du bist nun bei §b§lSmash§7!");
                         break;
-                    case "§8» §c§lTTT":
-                        player.teleport(new ConfigLocationUtil("TTT").loadLocation());
+                    case "§8» §a§lBuild §c§lBattle":
+                        player.teleport(new ConfigLocationUtil("BB").loadLocation());
                         player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 0.5f, 1);
-                        player.sendMessage(MessageManager.Prefix + "§7Du bist nun bei §c§lTTT§7!");
+                        player.sendMessage(MessageManager.Prefix + "§7Du bist nun bei §a§lBuild §c§lBattle§7!");
                         break;
                     case "§8» §9§lEnd§f§lOf§a§lLife":
                         player.teleport(new ConfigLocationUtil("EndOfLife").loadLocation());
                         player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 0.5f, 1);
                         player.sendMessage(MessageManager.Prefix + "§7Du bist nun bei §9§lEnd§f§lOf§a§lLife§7!");
+                        break;
+                    case "§8» §c§lMurder Mystery":
+                        player.teleport(new ConfigLocationUtil("MM").loadLocation());
+                        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 0.5f, 1);
+                        player.sendMessage(MessageManager.Prefix + "§7Du bist nun bei §c§lMurder Mystery§7!");
                         break;
                 }
             }
